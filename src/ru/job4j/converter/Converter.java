03e39f4f28d931fc1f32c3c317c1dc9d58d003a7
Maxim Azarkov <aza-maxim@yandex.ru>
2021-08-04 23:32:52 +0700

    0. Что такое тестирование [#159431]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,26 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        return value / 70;
+    }
+
+    public static float rubleToDollar(float value) {
+        return value / 60;
+    }
+
+    public static void main(String[] args) {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println(in + " rubles are " + out + " euro.");
+        System.out.println(in + " rubles are " + expected + " euro. Test result : " + passed);
+
+        in = 120;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println(in + " rubles are " + out + " dollar.");
+        System.out.println(in + " rubles are " + expected + " euro. Test result : " + passed);
+    }
+}
