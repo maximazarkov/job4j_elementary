906a1fc04b764283018e398bd4040992850c0320
Maxim Azarkov <aza-maxim@yandex.ru>
2021-08-11 00:21:15 +0700

    6.0.5. Массивы в тестах. [#226789]

---
+++ src/ru/job4j/array/Square.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class Square {
+    public static int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        /* заполним массив через цикл элементами от 1 до bound возведенными в квадрат */
+            for (int index = 1; index < rst.length; index++) {
+                rst[index] = (int) Math.pow(index,2);
+            }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        int[] array = calculate(4);
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
